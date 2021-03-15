01d1b1ef2d45a0a6d2cb9d62b89ba0942053505b
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2021-03-16 09:42:43 +1100

    django-storages use AWS CloudFront

--- config/s3_storages.py
+++ config/s3_storages.py
@@ -4,9 +4,11 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class MediaStorage(S3Boto3Storage):
     bucket_name = env.str("S3_RESOURCES_BUCKET_NAME", default=None)
+    custom_domain = env.str("AWS_S3_CUSTOM_DOMAIN", default=None)
     location = "media"
 
 
 class StaticStorage(S3Boto3Storage):
     bucket_name = env.str("S3_RESOURCES_BUCKET_NAME", default=None)
+    custom_domain = env.str("AWS_S3_CUSTOM_DOMAIN", default=None)
     location = "static"
