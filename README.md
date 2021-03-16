cef713c12f24d61e0fb6fc657c57281da591bef1
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2021-03-16 17:56:15 +1100

    Use Docker-Compose

--- README.md
+++ README.md
@@ -1,13 +1,31 @@
 ### This repo is the one-day-build of my professional portfolio website (www.findram.dev)
 
-__________
+---
 
 # Features
 
-+ Django 
-+ Wagtail CMS
-   * Richtext capability for blog section
-   * Streamfields and Orderedfields for adding new items (e.g. portfolio snippets & testimonials)
-+ HTML/CSS3
-+ Gunicorn / Nginx deployment on DigitalOcean
+- Django
+- Wagtail CMS
+  - Richtext capability for blog section
+  - Streamfields and Orderedfields for adding new items (e.g. portfolio snippets & testimonials)
+- HTML/CSS3
+- Gunicorn / Nginx deployment on DigitalOcean
 
+---
+
+# Docker instructions
+
+- Build
+
+```
+$ docker-compose build
+
+# Run manage.py commands
+$ docker-compose run app manage migrate
+```
+
+- Run
+
+```
+$ docker-compose up app
+```
---
+++ services/app/entrypoint.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+# http://stackoverflow.com/questions/19622198/what-does-set-e-mean-in-a-bash-script
+set -e
+
+# Check if the required PostgreSQL environment variables are set
+
+# Used by docker-entrypoint.sh to start the dev server
+# If not configured you'll receive this: CommandError: "0.0.0.0:" is not a valid port number or address:port pair.
+[ -z "$PORT" ] && echo "ERROR: Need to set PORT. E.g.: 8000" && exit 1;
+
+
+# Used by uwsgi.ini file to start the wsgi Django application
+[ -z "$WSGI_MODULE" ] && echo "ERROR: Need to set WSGI_MODULE. E.g.: config.wsgi:application" && exit 1;
+
+
+# Define help message
+show_help() {
+    echo """
+Usage: docker run <imagename> COMMAND
+
+Commands
+
+dev      : Start a normal Django development server
+bash     : Start a bash shell
+manage   : Start manage.py
+python   : Run a python command
+shell    : Start a Django Python shell
+uwsgi    : Run uwsgi server
+help     : Show this message
+"""
+}
+
+write_uwsgi() {
+    echo "Generating uwsgi config file..."
+    snippet="import os;
+import sys;
+import jinja2;
+sys.stdout.write(jinja2.Template(sys.stdin.read()).render(env=os.environ))"
+
+    cat /deployment/uwsgi.ini | python -c "${snippet}" > /uwsgi.ini
+}
+
+# Run
+case "$1" in
+    dev)
+        echo "Running Development Server on 0.0.0.0:${PORT}"
+        python manage.py runserver 0.0.0.0:${PORT}
+    ;;
+    bash)
+        /bin/bash "${@:2}"
+    ;;
+    manage)
+        python manage.py "${@:2}"
+    ;;
+    lint)
+        pylint "${@:2}"
+    ;;
+    python)
+        python "${@:2}"
+    ;;
+    shell)
+        python manage.py shell_plus
+    ;;
+    uwsgi)
+        echo "Running App (uWSGI)..."
+        write_uwsgi
+        uwsgi --ini /uwsgi.ini
+    ;;
+    *)
+        show_help
+    ;;
+esac
